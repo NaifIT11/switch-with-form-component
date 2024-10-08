a09acfcb4b4ea9b5115ae6a7c91c3e6754e02393
Naif Otibi <nn.n@outlook.cl>
2024-10-08 14:18:18 +0300

    initializing the form

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,8 +1,19 @@
 "use client"
 
+import { useState } from "react"
+
 
 export default function Form(){
+
+    const [name , setName] = useState('');
+    const [marketingEmails , setMarketingEmails] = useState(false);
+    function hanldeSubmit(){
+
+    }
+
     return (
-        <h1>hello</h1>
+        <form onSubmit={hanldeSubmit} className="space-y-4">
+
+        </form>
     )
 }
