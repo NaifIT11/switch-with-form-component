20469f987e8eefca4b6c389519443f43ca365ee4
Naif Otibi <nn.n@outlook.cl>
2024-10-08 14:22:47 +0300

    the form header

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -12,8 +12,14 @@ export default function Form(){
     }
 
     return (
-        <form onSubmit={hanldeSubmit} className="space-y-4">
-
-        </form>
+        <div className="space-y-4 p-4 rounded border">
+            <div className="space-y-2">
+                <h1 className="text-xl">Form</h1>
+                <p className="text-sm">please choose and enter data</p>
+            </div>
+            <form onSubmit={hanldeSubmit} className="space-y-4">
+                
+            </form>
+        </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Form from "./components/Form";
 
 export default function Home() {
   return (
-    <Form />
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <Form />
+    </div>
   );
 }
